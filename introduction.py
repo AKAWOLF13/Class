871295d2291b0b45f06aae5ba3095e4807f82e58
unknown <akawolf13@syuin.ac.kr>
2019-05-28 05:55:43 +0900

    first commit

---
+++ introduction.py
@@ -0,0 +1,30 @@
+
+class Introduce:
+
+    def name(self, name):
+        self.name = name
+    def age(self, age):
+        self.age = age
+    def major(self, major):
+        self.major = major
+    
+mj = Introduce()
+mj.name('이명재')
+mj.age('25')
+mj.major('경영정보학과')
+
+print("이름 : ", mj.name )
+print("나이 : ", mj.age)
+print("학과 : ", mj.major)
+            
+
+
+
+
+
+
+
+
+
+
+        
